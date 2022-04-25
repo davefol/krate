cba57fe582c2c4b1aea08932e9f9930bab481d31
Dave Fol <dof5@cornell.edu>
2022-04-25 02:02:07 -0400

    add summary stats for kview

--- krate/kview.py
+++ krate/kview.py
@@ -8,9 +8,14 @@ def main():
     for k, rating in ratings["choices"].items():
         elo.addPlayer(k, rating["rating"])
     power_rankings = sorted(elo.ratingDict.items(), key=lambda x:x[1], reverse=True)
-    print(f"{'Choice':<35}{'Rating':<20}")
+    rated = sum([r["matches"] for r in ratings["choices"].values()]) / 2
+    not_rated = len([r for r in ratings["choices"].values() if r["matches"] == 0])
+    print(f"You've judged {rated} match ups.")
+    print(f"You haven't rated {not_rated} choices.")
+    print(f"{'Choice':<35}{'Rating':<20}{'Matches':<20}")
     for k, r in power_rankings:
-        print(f"{k:<35}{r:<20.3f}")
+        m = ratings["choices"][k]["matches"]
+        print(f"{k:<35}{r:<20.3f}{m:<20}")
 
 if __name__ == "__main__":
     main()
